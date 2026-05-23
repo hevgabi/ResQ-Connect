204a41857b75fb47da2e0df22fb3e24afe77805a
klawi1121 <qcaoile@gmail.com>
2026-05-24 00:34:06 +0800

    setup for ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    GMSServices.provideAPIKey("AIzaSyCOooBNKqe-MshDk11ADPQQDK7k90W5Sl4")
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
